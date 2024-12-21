66bead83b71cce27f127559d978863da8759b663
ThanhfVux2102 <24560017@gm.uit.edu.vn>
2024-12-22 00:25:38 +0700

    sửa tí

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,7 @@
 // controllers/productController.js
 const Product = require('../models/Product');
 
-// // Lấy tất cả sản phẩm
-// exports.getProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find();
-//     res.render('layouts/products/products category/products', { products });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error retrieving products' });
-//   }
-// };
+
 
 // lấy beans:
 exports.getBeans = async (req, res) => {
@@ -79,17 +71,6 @@ exports.addProduct = async (req, res) => {
 };
 
 
-// // Cập nhật sản phẩm
-// exports.updateProduct = async (req, res) => {
-//   try {
-//     const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//     res.status(200).json(product);  // Trả về sản phẩm đã cập nhật dưới dạng JSON
-//   } catch (error) {
-//     res.status(400).json({ message: 'Error updating product' });
-//   }
-// };
-
-// Lấy thông tin sản phẩm để hiển thị form edit
 
 
 // Hiển thị form chỉnh sửa sản phẩm
