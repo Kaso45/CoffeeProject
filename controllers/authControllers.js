25fc8a8e2ef35134d94a0fd1a577070bce20d2dd
Henry <nhuquan.dev.py@gmail.com>
2024-12-25 02:12:53 +0700

    Add files via upload

---
+++ controllers/authControllers.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const  authController = {
+    //REGISTER
+    registerUser: async(req,res) => {
+        try{
+          const salt = await bcrypt.genSalt(10);
+          const hashed = await bcrypt.hash(req.body.password, salt);
+
+          //Create new user
+          const newUser  = await new User({
+            username: req.body.username,
+            password: hashed,
+          });
+
+          //Save to database
+          const user = await newUser.save();
+          res.status(200).json(user);
+          } catch(err){
+          res.status(500).json(err);
+          }
+    },
+    //LOGIN
+    loginUser: async(req, res)=>{
+        try{
+            const user = await User.findOne({username:req.body.username});
+            if(!user){
+                res.status(404).json("wrong username!");
+            }
+            const validPassword = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+            if(!validPassword) {
+                res.status(404).json('wrong password!');
+            }
+            if (user && validPassword){
+                res.status(200).json(user);
+            }
+        } catch(err){
+            res.status(500).json(err);
+        }
+    }
+};
+
+
+module.exports = authController;
