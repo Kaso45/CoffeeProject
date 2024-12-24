9456241cd99ded7070ea1e3bc9ed2626e154f8a4
Henry <nhuquan.dev.py@gmail.com>
2024-12-24 18:37:08 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const path = require(`path`);
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const authRoute = require('./routes/auth');
 const expressLayouts = require('express-ejs-layouts');
 const PORT = 3000;
 
 const route = require(`./routes/productRoutes`);
+const bcrypt = require('bcrypt');
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
 
 // import những thứ đã xuất ra từ /routes/index.js
 // const route = require('./routes');
 
 // Kết nối MongoDB
-mongoose.connect('mongodb+srv://project-management:12345nhom4@cluster0.kgyvd.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.log('MongoDB connection error: ', err);
-});
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 //tro toi file css
 app.use(express.static(path.join(__dirname, `public`)));
@@ -38,6 +45,50 @@ app.set('layout', 'layouts/main');
 // //dòng code là bao gồm tất cả routes của các trang (ctrl+click vô chữ route để dẫn tới /routes/index.js)
 // route(app);
 app.use(route)
+
+//trang home
+app.get(`/`, route);
+
+//Route
+app.use("v1/auth", authRoute);
+
+//contact
+app.get(`/contact`, route);
+
+//trang phan loai
+app.get(`/products`, route);
+  //trang beans
+  app.get(`/products/beans`, route);
+    // trang cua tung san pham
+    app.get(`/products/beans/aromacraft`, route);
+    app.get(`/products/beans/bitbrew`, route);
+    app.get(`/products/beans/casa`, route);
+    app.get(`/products/beans/koko`, route);
+    app.get(`/products/beans/navybrew`, route);
+    app.get(`/products/beans/zenbean`, route);
+  //trang capsules
+  app.get(`/products/capsules`, route);
+    //trang tung san pham
+    app.get(`/products/capsules/espresso`, route);
+    app.get(`/products/capsules/buno`, route);
+    app.get(`/products/capsules/heartblend`, route);
+    app.get(`/products/capsules/cosmo`, route);
+    app.get(`/products/capsules/pike`, route);
+    app.get(`/products/capsules/velvet`, route);
+  //trang grounds
+  app.get(`/products/grounds`, route);
+    //trang tung san pham
+    app.get(`/products/grounds/aromacraft-g`, route);
+    app.get(`/products/grounds/bitbrew-g`, route);
+    app.get(`/products/grounds/casa-g`, route);
+    app.get(`/products/grounds/koko-g`, route);
+    app.get(`/products/grounds/navybrew-g`, route);
+    app.get(`/products/grounds/zenbean-g`, route);
+app.get(`/profile`, route);
+app.get(`/cart`, route);
+app.get(`/brewguides`, route);
+app.get(`/login`, route);
+app.get(`/register`, route);
 
 // Khởi chạy server
 app.listen(PORT, () => {
